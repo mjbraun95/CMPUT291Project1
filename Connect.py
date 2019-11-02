b4a694e2154623c2392e661344434cd3aff057c8
Titaniayuki <57152556+Titaniayuki@users.noreply.github.com>
2019-11-01 22:35:35 -0600

    Connect

---
+++ Connect.py
@@ -0,0 +1,11 @@
+import sqlite3
+connection = None
+cursor = None
+def connect(path):
+    global connection, cursor
+
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    cursor.execute(' PRAGMA forteign_keys=ON; ')
+    connection.commit()
+    return
