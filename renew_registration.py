a5bce2916f948143f6050f01c7906d419f052d69
Titaniayuki <57152556+Titaniayuki@users.noreply.github.com>
2019-11-04 10:54:17 -0700

    3 more

---
+++ renew_registration.py
@@ -0,0 +1,30 @@
+#function 3
+# Renew a vehicle registration.The user should be able to provide an existing registration number and renew the registration.
+# The system should set the new expiry date to one year from today's date if the current registration either has expired
+# or expires today. Otherwise, the system should set the new expiry to one year after the current expiry date.
+def renew_vehicle_registration():
+    #get registration number
+
+    reg = None
+    regno = None
+    while not reg:
+        regno = input("Enter the registrations number of the registration to renew: ")
+        cursor.execute('SELECT * FROM registrations where regno = :regno ',
+                       {"regno": regno})
+        reg = cursor.fetchall()
+        if len(cursor.fetchall()) != 0:
+            reg = reg[0]
+
+    #set expiry to one year from today if expired or expiring today
+    year, month, day = reg[0][2].split('-')
+    expiry = datetime.date(int(year), int(month), int(day))
+    today = datetime.date.today()
+    if (expiry == today) or (expiry < today):
+        expiry = today.replace(year=today.year + 1)
+    else:
+        expiry = expiry.replace(year=expiry.year + 1)
+
+    #update table
+    cursor.execute('UPDATE registrations SET expiry = :expiry where regno = :regno;',
+                   {"expiry":expiry, "regno": regno})
+    connection.commit()
